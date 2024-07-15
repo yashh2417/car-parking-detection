8514b5c18ebd276dbbd9d2d5cc4c6aad612ad822
Yash <yashh2417@gmail.com>
2024-07-15 16:09:41 +0530

    first commit

---
+++ Object_Detection_App/app/src/main/java/com/example/imagepro/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.imagepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.opencv.android.OpenCVLoader;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d("MainActivity: ","Opencv is loaded");
+        }
+        else {
+            Log.d("MainActivity: ","Opencv failed to load");
+        }
+    }
+
+    private Button camera_button;
+    private Button storage_prediction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+                                                                                
+
+        // select device and run
+        // we successfully loaded model
+        // before next tutorial
+        // as we are going to predict in Camera Activity
+        // Next tutorial will be about predicting using Interpreter
+
+
+        camera_button=findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+        storage_prediction=findViewById(R.id.storage_prediction);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,StoragePredictionActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+    }
+}
---
+++ images/README.dataset.txt
@@ -0,0 +1,6 @@
+# car parking lot detection > 2023-09-19 9:19pm
+https://universe.roboflow.com/uttyler/car-parking-lot-detection
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
